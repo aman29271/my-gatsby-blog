96723787d16a61c8e96fd69b128dd3284d2dfeaf
Aman Kumar <aman29271@gmail.com>
2020-06-19 23:25:26 +0530

    removed logging

--- gatsby/onCreatePage.js
+++ gatsby/onCreatePage.js
@@ -4,7 +4,6 @@ module.exports = ({ page, actions }) => {
   const devContext = dev ? { draft: {} } : { draft: { eq: false } }
   const { createPage, deletePage } = actions
   if (page.path === "/blog/") {
-    console.log(devContext)
     deletePage(page)
     createPage({
       ...page,
